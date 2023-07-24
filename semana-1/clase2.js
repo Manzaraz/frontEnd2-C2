193842db7e113021526b0e3b32b3b3f9cfed5207
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-24 20:19:21 -0300

    Clase 01: Inicio

---
+++ semana-1/clase2.js
@@ -0,0 +1,41 @@
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 2                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Esta funcion nos devuelve 1, 2 o 3 según la elección del usuario.
+// Hasta que el usuario ingrese un dato válido le seguimos pidiendo que elija.
+
+function pedirJugada() {
+    // empezamos con la variable eleccion en 0
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 3                                 */
+/* -------------------------------------------------------------------------- */
+function jugadaRandom() {
+
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 4                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Esta funcion nos devuelve el resultado de la partida según las elecciones.
+// Comparamos la eleccion de cada uno para saber quien pierde y quien gana.
+
+function compararJugadas() {
+
+}
+
+const resultadoDePartida = compararJugadas()
+/* -------------------------------------------------------------------------- */
+/*                          CONSIGNA MESA DE TRABAJO                          */
+/* -------------------------------------------------------------------------- */
+// 1- Crear una función que reciba como parametro un texto (la frase de resultado de la partida).
+// 2- La función debe mostrar por consola el resultado de la partida.
+// 3- A su vez debe mostrar al usuario una alerta con el resutado de la partida.
+// 4- Finalmente, si el resultado fue una derrota debe mostrarle al usuario un mensaje de aliento para desearle suerte en la próxima oportunidad.
+
+
+
