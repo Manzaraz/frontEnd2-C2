bf7482aab2b96a374a1f0c29c122b9b19dda304c
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-28 21:23:23 -0300

    Clase 02: Inicio de clase

--- semana-1/clase1.js
+++ semana-1/clase1.js
@@ -1,23 +1,53 @@
 /* -------------------------------------------------------------------------- */
 /*                                  FUNCION 1                                 */
 /* -------------------------------------------------------------------------- */
+// function iniciarJuego() {
+//     // Saludamos al vistante
+//     alert("Bienvenido al juego de Piedra, Pael o Tijera de Frontend 2.!");
+//     // Guardar el nombre en una variable
+//     const nombre = prompt("Su nombre");
+
+//     // alert("Hola " + nombre + ".")
+//     alert(`¡Hola estimado: ${nombre}, Mucha suerte!`)
+
+//     // Mostramos los datos en consola
+//     console.log("---------------------------");
+//     console.log("El nombre del jugador es: ");
+//     console.log(nombre);
+//     console.log("---------------------------");
+
+//     return nombre
+// }
+
+// ***************************
+// Equipo 2
+// ***************************
+
+
 function iniciarJuego() {
-    // Saludamos al vistante
-    alert("Bienvenido al juego de Piedra, Pael o Tijera de Frontend 2.!");
-    // Guardar el nombre en una variable
-    const nombre = prompt("Su nombre");
+    let ok = false;
+    // suludamos al usuario
+    alert("Bienvenido al piedra papel o tijera de Frontend II.");
 
-    // alert("Hola " + nombre + ".")
-    alert(`¡Hola estimado: ${nombre}, Mucha suerte!`)
-
-    // Mostramos los datos en consola
-    console.log("---------------------------");
-    console.log("El nombre del jugador es: ");
-    console.log(nombre);
-    console.log("---------------------------");
-
-    return nombre
+    do {
+        // guardamos en una variable en nombre ingresado
+        const nombre = prompt("Ingese su nombre por favor:").toUpperCase()
+        if (!isNaN(nombre) || nombre.length <= 3) {
+            alert("Tu nombre debe tener mas de 3 caracteres y no se permiten numeros");
+            ok = true;
+        } else {
+            ok = false
+            alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
+            // mostramos los datos por consola
+            console.log("----------------------------");
+            console.log("El jugador es:")
+            console.log(nombre);
+            console.log("----------------------------");
+            return nombre;
+        }
+    } while (ok == true)
 }
+
 
 // creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
 let nombreJugador = iniciarJuego();
