fbb5ce3a3b2ec9057d8c078a3669b050d07d7c9d
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-31 20:51:24 -0300

    Clase 03: Inicio de clase

--- semana-2/clase5.js
+++ semana-2/clase5.js
@@ -3,42 +3,16 @@
 // Vamos a interactuar con el DOM para agregarle mas estilos a nuestro sitio.
 // 👇 Primero capturemos todos los elementos que vamos a modificar.
 
-const sitio = document.querySelector('body');
-const btnTema = document.querySelector('.tema button')
-const menuItems =  document.querySelectorAll('nav li');
-const contenedorNoticias = document.querySelector('main');
-const articulos = document.querySelectorAll('article');
-const titulos = document.querySelectorAll('article h2');
+
 
 /* ---------------------------- Editado los ITEMS --------------------------- */
 // 👇acá podemos ver todas las propiedades CSS que podemos modificar con JS
-console.log(menuItems[0].style)
-// agregamos manualmente nuevos estilos en el menú
-menuItems.forEach( item => {
-    item.style.textTransform = "uppercase";
-    item.style.color = "aqua";
-    item.style.backgroundColor = "rgba(255,255,255,0.2";
-    item.style.borderRadius = "50vh";
-})
 
-console.log(contenedorNoticias)
+// agregamos manualmente nuevos estilos en el menú
 
 /* ----------------------------- Editando clases ---------------------------- */
 
 // vamos probando uno a uno los métodos
-console.log(sitio.classList)
-
-console.log(sitio.classList.contains('dark'))
-console.log(sitio.classList)
-
-console.log(sitio.classList.add('dark'))
-console.log(sitio.classList)
-
-console.log(sitio.classList.add('remove'))
-console.log(sitio.classList)
-
-console.log(sitio.classList.toggle('remove'))
-console.log(sitio.classList)
 
 
 
@@ -51,6 +25,6 @@ console.log(sitio.classList)
 // 3- Si el usuario confirma debemos aplicar la clase "dark" al "sitio", si cancela debe quedar en modo claro.
 // 4- A su vez, si está en modo onsecuritypolicyviolation, el texto del boton debe decir "Cambiar a modo claro 🌞". De lo contrario, si está en modo claro debeb decir "Cambiar a modo oscuro 🌛"
 function elegirTema() {
-    
+
 }
 elegirTema();
